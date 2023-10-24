485259f0e507848f055a4075ec4df35135b4e26a
Carlos Calderón del Cid <caldecid@gmail.com>
2023-10-24 17:38:40 -0300

    expowo with synonyms

--- Scripts/new_expowo.R
+++ Scripts/new_expowo.R
@@ -1,14 +1,17 @@
+library(tidyverse)
+library(expowo)
+library(plyr)
 
 ###########expowo##########
 
 powo_fam <- unique(df_powo_families$family)
 
-list_expowo <- vector("list", length = length(powo_fam))
+list_expowo <- vector("list", length = length(powo_fam) -99)
 
-names(list_expowo) = powo_fam
+names(list_expowo) = powo_fam[100:142]
 
-for(i in 18:length(powo_fam)){
-  ##filetring family
+for(i in 100:length(powo_fam)){
+  ##filtering family
   fam_powo = df_powo_families %>% filter(family == powo_fam[i])
   
   ##extracting genus
@@ -45,6 +48,41 @@ for(i in 18:length(powo_fam)){
   
   }
   
+   
+  list_expowo[[i]] = do.call("rbind.fill", list_genus)
  
-  list_expowo[[i]] = do.call("rbind", list_genus)
 }
+
+
+
+list_expowo <- list_expowo[-which(sapply(list_expowo, is.null))]
+
+
+list_expowo <- list_expowo[-which(sapply(list_expowo,
+                                         function(x) dim(x)[1] == 0))]
+
+
+save(list_expowo, file = "Data/list_expowo.RData")
+
+write_csv(list_expowo[[i]], 
+          file = paste0("Data/Metadata/Angiosperms/expowo/expowo_syn/",
+                        powo_fam[i], ".csv"))
+for(i in 45:length(list_expowo)) {
+  write_xlsx(list_expowo[[i]], 
+            path = paste0("Data/Metadata/Angiosperms/expowo/expowo_syn/",
+                          unique(list_expowo[[i]]$family), ".xlsx"))
+  print(unique(list_expowo[[i]]$family))
+}
+
+##xlsx has some limits
+write_csv(list_expowo[[44]], 
+                     file = paste0("Data/Metadata/Angiosperms/expowo/expowo_syn/",
+                                   unique(list_expowo[[44]]$family), ".csv"))
+
+x = vector(mode = "character", length = length(list_expowo))
+
+for(i in seq_along(list_expowo)){
+  x[i] = unique(list_expowo[[i]]$family)
+}
+
+
