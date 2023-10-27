e8e60e15f1f5c4dc2900dc7e91b7056e7fcc7ec5
Carlos Calderón del Cid <caldecid@gmail.com>
2023-10-27 15:35:06 -0300

    expowo datasets

--- Scripts/new_expowo.R
+++ Scripts/new_expowo.R
@@ -6,11 +6,14 @@ library(plyr)
 
 powo_fam <- unique(df_powo_families$family)
 
-list_expowo <- vector("list", length = length(powo_fam) -99)
+##second round
+powo_fam = powo_fam[100:142]
+list_expowo <- vector("list", length = length(powo_fam))
 
-names(list_expowo) = powo_fam[100:142]
+names(list_expowo) = powo_fam
 
-for(i in 100:length(powo_fam)){
+
+for(i in seq_along(powo_fam)){
   ##filtering family
   fam_powo = df_powo_families %>% filter(family == powo_fam[i])
   
@@ -67,7 +70,7 @@ save(list_expowo, file = "Data/list_expowo.RData")
 write_csv(list_expowo[[i]], 
           file = paste0("Data/Metadata/Angiosperms/expowo/expowo_syn/",
                         powo_fam[i], ".csv"))
-for(i in 45:length(list_expowo)) {
+for(i in 21:length(list_expowo)) {
   write_xlsx(list_expowo[[i]], 
             path = paste0("Data/Metadata/Angiosperms/expowo/expowo_syn/",
                           unique(list_expowo[[i]]$family), ".xlsx"))
@@ -75,9 +78,9 @@ for(i in 45:length(list_expowo)) {
 }
 
 ##xlsx has some limits
-write_csv(list_expowo[[44]], 
+write_csv(list_expowo[[20]], 
                      file = paste0("Data/Metadata/Angiosperms/expowo/expowo_syn/",
-                                   unique(list_expowo[[44]]$family), ".csv"))
+                                   unique(list_expowo[[20]]$family), ".csv"))
 
 x = vector(mode = "character", length = length(list_expowo))
 
